f782d27e745f78565493f79fe35276174474a4a7
zeeshan zahoor <105010948+zeeshanzahoor786@users.noreply.github.com>
2024-06-17 09:41:41 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.474d2ae8174ad0ce54cd09eff4d56e40.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997836802f3068045f0ebd3981eabe8f",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/index.html"
+  },
+  {
+    "revision": "a2465af4c7eaadaf02e1",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/css/main.10529e77.chunk.css"
+  },
+  {
+    "revision": "fc9c775eeddfa46bbf49",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/js/0.8c1343d3.chunk.js"
+  },
+  {
+    "revision": "d04216c6afe51701bb56",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/js/3.5dbbca38.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/js/3.5dbbca38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa79918bf770cd2d4ec0",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/js/4.5580f459.chunk.js"
+  },
+  {
+    "revision": "a2465af4c7eaadaf02e1",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/js/main.173d0c72.chunk.js"
+  },
+  {
+    "revision": "9fe0c79c790e4ead90be",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/js/runtime-main.47c7ee1c.js"
+  },
+  {
+    "revision": "b6f21244a120a65e8247f3640aa8c8ea",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/bgVideo.b6f21244.mp4"
+  },
+  {
+    "revision": "558a8275c6eac3fdc43ba760c2480ddd",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/classFour.558a8275.jpg"
+  },
+  {
+    "revision": "17401abc39bf9b5408b0c49aae1535d4",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/classOne.17401abc.jpg"
+  },
+  {
+    "revision": "c5162d4d69ce41a174eb4f456500d812",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/classThree.c5162d4d.jpg"
+  },
+  {
+    "revision": "92e684d5da73e14ef390d0ff620fb8c5",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/classTwo.92e684d5.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/gymOverlayBg.f042c2ac.jpg"
+  },
+  {
+    "revision": "7afa204f6ab579e2ffccf7f63cead7fd",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/scheduleBg.7afa204f.jpg"
+  },
+  {
+    "revision": "9af6560400ef14efd6bb74bfb2313b74",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/trainerOne.9af65604.jpg"
+  },
+  {
+    "revision": "7bfc49e84503eed5ae51943d65d3b6a9",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/trainerThree.7bfc49e8.jpg"
+  },
+  {
+    "revision": "c7a6feb7730a8818c3f4297f33a6896c",
+    "url": "/zeeshanzahoor786/imgly-deployment.git/static/media/trainerTwo.c7a6feb7.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zeeshanzahoor786/imgly-deployment.git/precache-manifest.474d2ae8174ad0ce54cd09eff4d56e40.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zeeshanzahoor786/imgly-deployment.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
